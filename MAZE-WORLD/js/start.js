774123511e705f8aa95facf2bafb0533bed46a78
AlexandreMorales <xandemorales@gmail.com>
2024-11-28 02:23:10 -0300

    prevent default zoom

--- MAZE-WORLD/js/start.js
+++ MAZE-WORLD/js/start.js
@@ -37,6 +37,7 @@ document.ontouchstart = (e) => {
   const { screenX, screenY } = e.touches[0];
 
   if (e.touches.length === 2) {
+    e.preventDefault();
     zoomDist = Math.hypot(
       screenX - e.touches[1].screenX,
       screenY - e.touches[1].screenY
@@ -49,6 +50,7 @@ document.ontouchmove = (e) => {
   const { screenX, screenY } = e.touches[0];
 
   if (e.touches.length === 2) {
+    e.preventDefault();
     const nZoomDist = Math.hypot(
       screenX - e.touches[1].screenX,
       screenY - e.touches[1].screenY
