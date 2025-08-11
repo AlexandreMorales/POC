f8b2270340b94074d8ed08d34047a0cdc886c757
AlexandreMorales <xandemorales@gmail.com>
2025-08-11 19:08:15 -0300

    refactor make minify compression better

--- MAZE-WORLD/scripts/utils.js
+++ MAZE-WORLD/scripts/utils.js
@@ -5,23 +5,49 @@ const fs = require("fs");
  * @param {string} extension
  * @param {string} output
  */
-const readFile = (filePath, extension, output) => {
+const writeMinFile = (filePath, extension, output) => {
+  // Ignore index and JSDocs files
   if (
     filePath.endsWith(`index.${extension}`) ||
-    filePath.endsWith(`interfaces.${extension}`) ||
-    filePath.endsWith(`min.${extension}`)
+    filePath.endsWith(`interfaces.${extension}`)
   )
     return;
 
   try {
     let data = fs.readFileSync(filePath, "utf8");
     data = data
+      // Remove exports
       .replaceAll("export ", "")
+      // Remove imports
       .replaceAll(/import.+?from ".+?";/gs, "")
+      // Remove JSDocs
       .replaceAll(/\/\*\*.+?\*\//gs, "")
+      // Remove comments
       .replaceAll(/\/\/.+?\n/gs, "")
-      .replaceAll(/\n/gs, "")
-      .replaceAll(/\s\s/gs, "");
+      // Remove break lines
+      .replaceAll(/\n/gs, "");
+
+    if (extension == "js")
+      data = data
+        // Remove spaces that are not between words or quotes
+        .replaceAll(
+          /(?=(?<!\w))\s+|\s+(?=(?!\w))(?=(?:[^'"`]*(['"`])[^'"`]*\1)*[^'"`]*$)/gi,
+          ""
+        );
+    else
+      data = data
+        // Remove identation
+        .replaceAll(/\s+/gi, " ")
+        .replaceAll(/\/\s+/gi, "/")
+        .replaceAll(/\s+\//gi, "/")
+        .replaceAll(/\>\s+/gi, ">")
+        .replaceAll(/\s+\>/gi, ">")
+        .replaceAll(/\{\s+/gi, "{")
+        .replaceAll(/\s+\{/gi, "{")
+        .replaceAll(/\,\s+/gi, ",")
+        .replaceAll(/\;\s+/gi, ";")
+        .replaceAll(/\:\s+/gi, ":");
+
     fs.appendFileSync(output, data, "utf8");
   } catch (err) {
     return;
@@ -29,22 +55,35 @@ const readFile = (filePath, extension, output) => {
 };
 
 /**
- * @param {string} folderPath
- * @param {string[]} filesToGoFirst
+ * @param {string[]} dirPaths
  * @param {string} extension
  * @param {string} output
  */
-const readFolder = (folderPath, filesToGoFirst, extension, output) => {
-  filesToGoFirst.forEach((f) =>
-    readFile(`${folderPath}/${f}`, extension, output)
-  );
-  fs.readdirSync(folderPath).forEach((dirPath) => {
-    dirPath = `${folderPath}/${dirPath}`;
-    if (dirPath.endsWith(extension)) {
-      if (filesToGoFirst.find((f) => dirPath.endsWith(f))) return;
-      readFile(dirPath, extension, output);
-    } else readFolder(dirPath, filesToGoFirst, extension, output);
+const readDirToMinify = (dirPaths, extension, output) => {
+  dirPaths.forEach((dirPath) => {
+    if (dirPath.endsWith(extension)) writeMinFile(dirPath, extension, output);
+    else readFolderToMinify(dirPath, extension, output);
   });
 };
 
-module.exports = { readFile, readFolder };
+/**
+ * @param {string} folderPath
+ * @param {string} extension
+ * @param {string} output
+ */
+const readFolderToMinify = (folderPath, extension, output) => {
+  const dirs = fs.readdirSync(folderPath);
+  const normalDirs = [];
+  const firstDirs = [];
+
+  // Always read files and folders with _ in the front
+  dirs.forEach((dirPath) => {
+    const fullPath = `${folderPath}/${dirPath}`;
+    if (dirPath.startsWith("_")) firstDirs.push(fullPath);
+    else normalDirs.push(fullPath);
+  });
+  readDirToMinify(firstDirs, extension, output);
+  readDirToMinify(normalDirs, extension, output);
+};
+
+module.exports = { readFolderToMinify };
